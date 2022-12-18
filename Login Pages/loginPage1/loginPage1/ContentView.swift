35ed8d0d250d2460ca8fa97717b2b1d4d6596ea5
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-12-18 04:00:17 +0300

    Navigation and Auth

--- Login Pages/loginPage1/loginPage1/ContentView.swift
+++ Login Pages/loginPage1/loginPage1/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var password = ""
     @State private var wrongUserName = 0
     @State private var wrongPassword = 0
+    @State private var showingLoginScree = false
     
     var body: some View {
         NavigationView () {
@@ -52,12 +53,17 @@ struct ContentView: View {
                         .border(.red, width: CGFloat(wrongPassword))
 
                     Button("Login") {
-                        
+                        autheticateUser(username: username, password: password)
                     }
                     .foregroundColor(.white)
                     .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.height * 0.06)
                     .background(Color.red)
                     .cornerRadius(10)
+                    
+                    NavigationLink (destination: Text("you are logged in @\(username)") , isActive : $showingLoginScree) {
+                        EmptyView()
+                    }
+
 
                     
                     
@@ -67,11 +73,27 @@ struct ContentView: View {
             }
             
             .toolbar(.hidden, for: .tabBar)
+
             
             
         }
        
     
+    }
+    
+    func autheticateUser(username : String, password : String){
+        if username.lowercased() == "ozancicek" {
+            wrongUserName = 0
+            if password.lowercased() == "123456" {
+                wrongPassword = 0
+                showingLoginScree = true
+            }else {
+                wrongPassword = 2
+            }
+        }else {
+            wrongUserName = 2
+        }
+        
     }
 }
 
