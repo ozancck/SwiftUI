8586ab9516554bfae1ea1ce967b5d2f635a7d094
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-02-22 19:19:51 +0300

    AppleWidgetApp
    Model

---
+++ AppleWidgetApp/AppleWidgetApp/AppleWidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWidgetAppApp.swift
+//  AppleWidgetApp
+//
+//  Created by Ozan Çiçek on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleWidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleWidgetApp/AppleWidgetApp/SuperApple.swift
@@ -0,0 +1,8 @@
+//
+//  SuperApple.swift
+//  AppleWidgetApp
+//
+//  Created by Ozan Çiçek on 22.02.2023.
+//
+
+import Foundation
