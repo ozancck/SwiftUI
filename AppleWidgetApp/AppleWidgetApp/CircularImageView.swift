ce8710257e4c148e4cf4a6bb70a794343a25602a
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-02-22 20:05:46 +0300

    AppleWidgetAPP
    CIRCULAR VIEW UPDATE

--- AppleWidgetApp/AppleWidgetApp/CircularImageView.swift
+++ AppleWidgetApp/AppleWidgetApp/CircularImageView.swift
@@ -8,13 +8,51 @@
 import SwiftUI
 
 struct CircularImageView: View {
+    
+    var image : String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+           
+        
+        ZStack {
+            
+            Circle().fill(Color.gray)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.4)
+                .shadow(radius: 25)
+            
+            
+            Image(image)
+                .resizable()
+                .scaleEffect(0.7)
+                .aspectRatio(contentMode: .fit)
+               
+                .clipShape(Circle())
+               
+                .overlay(Circle()
+                    .stroke(Color.black, lineWidth: 5)
+                )
+                .shadow(radius: 15)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.4)
+           
+            
+        }
+               
+                
+                    
+      
+        
+        
+        
+       
+            
+            
+            
     }
 }
 
 struct CircularImageView_Previews: PreviewProvider {
     static var previews: some View {
-        CircularImageView()
+        CircularImageView(image: "macbook")
+            .previewLayout(.fixed(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.5))
     }
 }
--- AppleWidgetApp/AppleWidgetApp/SuperApple.swift
+++ AppleWidgetApp/AppleWidgetApp/SuperApple.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct SuperApple: Identifiable, Codable {
+    let image: String
+    let name: String
+    let model: String
+
+    var id: String { image }
+}
+
+let macbook = SuperApple(image: "macbook", name: "Macbook", model: "Macbook Pro M1")
+
+let appleW = SuperApple(image: "appleW", name: "Apple W", model: "SE")
+
+let iphone = SuperApple(image: "iphone", name: "Iphone", model: "Iphone 11")
+
+
