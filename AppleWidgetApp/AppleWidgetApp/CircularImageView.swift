4fc28a4382a9562598bdc93d5c21c59177f0e7a5
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-02-22 19:45:52 +0300

    AppleWidgetApp CircularView

---
+++ AppleWidgetApp/AppleWidgetApp/CircularImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularImageView.swift
+//  AppleWidgetApp
+//
+//  Created by Ozan Çiçek on 22.02.2023.
+//
+
+import SwiftUI
+
+struct CircularImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircularImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularImageView()
+    }
+}
