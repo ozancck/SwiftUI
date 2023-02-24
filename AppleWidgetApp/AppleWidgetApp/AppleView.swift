b3ce672f741ad4ba6278197b6a0bfe1e71accf6c
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-02-24 20:51:03 +0300

    AppleWidgetApp AppleWiew

---
+++ AppleWidgetApp/AppleWidgetApp/AppleView.swift
@@ -0,0 +1,20 @@
+//
+//  AppleView.swift
+//  AppleWidgetApp
+//
+//  Created by Ozan Çiçek on 24.02.2023.
+//
+
+import SwiftUI
+
+struct AppleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppleView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppleView()
+    }
+}
