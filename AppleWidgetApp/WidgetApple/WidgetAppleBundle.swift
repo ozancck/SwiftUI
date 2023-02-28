e62f2f5aca2a9b05f2db82fd9fce309c35debeb8
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2023-02-28 19:37:12 +0300

    Apple Widget App
    done

---
+++ AppleWidgetApp/WidgetApple/WidgetAppleBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppleBundle.swift
+//  WidgetApple
+//
+//  Created by Ozan Çiçek on 28.02.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetAppleBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetApple()
+        WidgetAppleLiveActivity()
+    }
+}
---
+++ AppleWidgetApp/WidgetApple/WidgetAppleLiveActivity.swift
@@ -0,0 +1,57 @@
+//
+//  WidgetAppleLiveActivity.swift
+//  WidgetApple
+//
+//  Created by Ozan Çiçek on 28.02.2023.
+//
+
+import ActivityKit
+import WidgetKit
+import SwiftUI
+
+struct WidgetAppleAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        // Dynamic stateful properties about your activity go here!
+        var value: Int
+    }
+
+    // Fixed non-changing properties about your activity go here!
+    var name: String
+}
+
+struct WidgetAppleLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: WidgetAppleAttributes.self) { context in
+            // Lock screen/banner UI goes here
+            VStack {
+                Text("Hello")
+            }
+            .activityBackgroundTint(Color.cyan)
+            .activitySystemActionForegroundColor(Color.black)
+            
+        } dynamicIsland: { context in
+            DynamicIsland {
+                // Expanded UI goes here.  Compose the expanded UI through
+                // various regions, like leading/trailing/center/bottom
+                DynamicIslandExpandedRegion(.leading) {
+                    Text("Leading")
+                }
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text("Trailing")
+                }
+                DynamicIslandExpandedRegion(.bottom) {
+                    Text("Bottom")
+                    // more content
+                }
+            } compactLeading: {
+                Text("L")
+            } compactTrailing: {
+                Text("T")
+            } minimal: {
+                Text("Min")
+            }
+            .widgetURL(URL(string: "http://www.apple.com"))
+            .keylineTint(Color.red)
+        }
+    }
+}
