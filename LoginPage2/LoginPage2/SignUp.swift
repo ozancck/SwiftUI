571ba5349cb634993fe1882fa4687e5ec78e007c
U200709077 <ozancck@icloud.com>
2023-03-03 20:12:31 +0300

    loginPage2
    sign up view

---
+++ LoginPage2/LoginPage2/SignUp.swift
@@ -0,0 +1,20 @@
+//
+//  SignUp.swift
+//  LoginPage2
+//
+//  Created by Ozan Çiçek on 3.03.2023.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
+}
