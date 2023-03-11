88463dbf407e468e2206c83b3c821cc28231e0c4
U200709077 <ozancck@icloud.com>
2023-03-11 04:07:01 +0300

    JokesApp AnswerView

---
+++ JokesApp/JokesApp/AnswerView.swift
@@ -0,0 +1,20 @@
+//
+//  AnswerViews.swift
+//  JokesApp
+//
+//  Created by Ozan Çiçek on 11.03.2023.
+//
+
+import SwiftUI
+
+struct AnswerViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnswerViews_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswerViews()
+    }
+}
