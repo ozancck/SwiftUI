d2e26501c26ec55cf49fe136fd7e35e7b5359b0f
U200709077 <ozancck@icloud.com>
2023-03-18 14:35:49 +0300

    JokesApp Modifiers
    Button and Text modifiers

---
+++ JokesApp/JokesApp/ButtonModifier.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonModifier.swift
+//  JokesApp
+//
+//  Created by Ozan Çiçek on 18.03.2023.
+//
+
+import Foundation
---
+++ JokesApp/JokesApp/TextHeaderModifier.swift
@@ -0,0 +1,8 @@
+//
+//  TextHeaderModifier.swift
+//  JokesApp
+//
+//  Created by Ozan Çiçek on 18.03.2023.
+//
+
+import Foundation
